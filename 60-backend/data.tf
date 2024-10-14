1f3518fdc07d6020982b59097b52aafadf556cd8
hemanth_kumar <hemanth4125@gmail.com>
2024-10-14 17:28:25 +0530

    infra development

--- 60-backend/data.tf
+++ 60-backend/data.tf
@@ -6,6 +6,10 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.envinronment}/private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.envinronment}/vpc_id"
+}
+
 data "aws_ssm_parameter" "backend_sg_id" {
   name = "/${var.project}/${var.envinronment}/backend_sg_id"
 }
--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -80,3 +80,81 @@ resource "null_resource" "backend_delete" {
 
   depends_on = [ aws_ami_from_instance.backend ]
 }
+
+resource "aws_lb_target_group" "backend" {
+  name     = local.resource_name
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    matcher = "200-299"
+    path = "/Health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 4
+  }
+}
+
+resource "aws_launch_template" "backend" {
+  name = local.resource_name
+  image_id = aws_ami_from_instance.backend.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [local.backend_sg_id]
+  update_default_version = true
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = local.resource_name
+    }
+  }
+
+  # user_data = filebase64("${path.module}/example.sh")
+}
+
+resource "aws_autoscaling_group" "backend" {
+  name                      = local.resource_name
+  max_size                  = 10
+  min_size                  = 2
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  # force_delete              = true
+  launch_template {
+    id = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier       = [local.private_subnet_id]
+
+    tag {
+    key                 = "Name"
+    value               = local.resource_name
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "project"
+    value               = "Expense"
+    propagate_at_launch = false
+  }
+}
+
+resource "aws_autoscaling_policy" "backend" {
+  autoscaling_group_name = aws_autoscaling_group.backend.name
+  name = local.resource_name
+  policy_type = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 70.0
+  }
+}
